771007289bd5ec7d400e0483e7dc7471aa22079c
Artem Kovalev <a.kovalev@gaijin.ru>
2019-03-22 02:10:21 +0300

    build(package build): rpm and python packages builder
    Added scripts for build rpm and python (whl/gz) packages via Docker.

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM centos:7
+
+LABEL name="zstlib" \
+  version="0.0.1.0"
+
+ENV PYTHON_VERSION="3.7.1"
+ENV LD_LIBRARY_PATH="/usr/lib:/usr/lib64"
+
+# Install base/develop packages
+RUN true \
+  && yum install -y epel-release \
+  && yum group install -y "Development Tools" \
+  && yum install -y ruby-devel gem libffi-devel libsqlite3x-devel \
+    sqlite-devel wget zlib-devel expat-devel python2-pip \
+    openssl openssl-devel tree \
+  && yum -y clean all \
+  && echo "gem: --no-ri --no-rdoc --no-document" > ~/.gemrc \
+  && gem install fpm \
+  && gem cleanup all \
+  && true
+
+# Install python and dependencies
+COPY build/python-build.sh /python-build.sh
+RUN chmod +x /python-build.sh; /python-build.sh; \
+    /bin/python -m pip install wheel; \
+    /bin/python3 -m pip install wheel
+
+# Prepare build directory
+WORKDIR /build
+COPY build/rpm-build.sh /build/rpm-build.sh
+RUN chmod +x /build/rpm-build.sh
---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+programming_language = os.getenv('PROG_LANG_VERSION', 2)
+
+if os.getenv('WITH_DEPENDENCIES', False):
+    with open('requirements.txt', 'r') as req_file:
+        args = {
+            'install_requires': req_file.read().split('\n'),
+        }
+else:
+    args = {}
+
+setuptools.setup(
+    name='zstlib',
+    version='0.0.1.0',
+    author='zombig',
+    author_email='zstlib@zombig.name',
+    description='Small useful python tool set',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/zombig/zstlib',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: {}'.format(programming_language),
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    **args
+)
