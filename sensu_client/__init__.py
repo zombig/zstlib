0d626f898c55297ecd448ca778f3ad15902123df
zombig <me@zombig.name>
2019-03-24 19:29:39 +0300

    feat(sensu client): implement simple sensu client
    The client can send ok, warn, error and unknow messages as class methods

---
+++ sensu_client/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from sensu_client import SensuClient
+
+__all__ = ['SensuClient', ]
---
+++ sensu_client/sensu_client.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import json
+import socket
+import time
+
+
+class SensuClient:
+
+    OK = 0
+    WARNING = 1
+    ERROR = 2
+    UNKNOWN = 3
+
+    def __init__(self, name, address='localhost', port=3030, command='unknown', handlers=['default'], **kwargs):
+        self.name = name
+        self.address = address
+        self.port = port
+        self.handlers = handlers
+        self.command = command
+
+    def __send(self, message):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.address, self.port))
+        s.send(message)
+
+    def __compose(self, output, status):
+        message = {
+            'name': self.name,
+            'issued': int(time.time()),
+            'output': output.strip(),
+            'status': status,
+            'command': self.command,
+            'handlers': self.handlers,
+        }
+        return json.dumps(message)
+
+    def ok(self):
+        self.__send(self.__compose('OK', self.OK))
+
+    def warning(self, message):
+        self.__send(self.__compose(message, self.WARNING))
+
+    def error(self, message):
+        self.__send(self.__compose(message, self.ERROR))
+
+    def unknown(self, message):
+        self.__send(self.__compose(message, self.UNKNOWN))
+
+
+if __name__ == '__main__':
+    assert False, 'ERROR: Direct access to this file is not allowed!'
