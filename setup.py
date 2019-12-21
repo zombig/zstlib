5d35291c6c8311ed7d04c6ab617c47145c5e0fa2
zombig <me@zombig.name>
2019-12-21 14:29:09 +0300

    fix(setup.py): fix version control
    Now we're getting version from package json file, that keep current release version.
    
    fix #3

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import os
 
 import setuptools
@@ -6,9 +7,9 @@ import setuptools
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-programming_language = os.getenv('PROG_LANG_VERSION', 2)
+programming_language = os.getenv('PROG_LANG_VERSION', '2')
 
-if os.getenv('WITH_DEPENDENCIES', False):
+if os.getenv('WITH_DEPENDENCIES', None):
     with open('requirements.txt', 'r') as req_file:
         args = {
             'install_requires': req_file.read().split('\n'),
@@ -16,9 +17,20 @@ if os.getenv('WITH_DEPENDENCIES', False):
 else:
     args = {}
 
+
+def get_version():
+    """
+    Read version from package json file,
+    that is used for keep current
+    :return: str(version)
+    """
+    with open('package.json') as json_file:
+        return json.load(json_file)['version']
+
+
 setuptools.setup(
     name='zstlib',
-    version='0.1.1.1',
+    version=get_version(),
     author='zombig',
     author_email='zstlib@zombig.name',
     description='Small useful python tool set',
